524a116eaaaa259efd51bc02e545d49d0175da1a
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2022-11-29 09:25:16 -0700

    finished daily code wars

--- index.js
+++ index.js
@@ -42,3 +42,24 @@ console.log(tribonacci([], 10));
 console.log(tribonacci([1, 1, 1], 0));
 console.log(tribonacci([1, 1, 1], 1));
 console.log(tribonacci([1, 1, 1], 2));
+
+
+// Problem 2
+
+// Story
+// Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+// Task
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+
+function minMax(arr){
+  let answer =[]
+  answer.push(Math.min(...arr), Math.max(...arr))
+  // console.log(answer)
+  return answer
+}
+
+console.log(minMax([1, 2, 3, 4, 5]))
+console.log(minMax([2334454, 5]))
+console.log(minMax([5]))
