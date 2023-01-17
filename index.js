dd7990f6ddc93ee607ca8f6dd24faf3cc100b717
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-01-17 09:37:18 -0700

    finished problem 12

--- index.js
+++ index.js
@@ -25,12 +25,15 @@ function tribonacci(signature, n) {
   if (n === 1) {
     return [signature[0]];
   } else if (n === 2) {
-    return [signature[0], signature[1]]
+    return [signature[0], signature[1]];
   }
   result.push(...signature);
   // console.log(result)
   for (let i = 0; i < n - 3; i++) {
-    newSum = result[result.length - 1] + result[result.length - 3] + result[result.length - 2];
+    newSum =
+      result[result.length - 1] +
+      result[result.length - 3] +
+      result[result.length - 2];
     // console.log(newSum)
     result.push(newSum);
   }
@@ -43,7 +46,6 @@ function tribonacci(signature, n) {
 // console.log(tribonacci([1, 1, 1], 1));
 // console.log(tribonacci([1, 1, 1], 2));
 
-
 // Problem 2
 
 // Story
@@ -52,65 +54,59 @@ function tribonacci(signature, n) {
 // Task
 // Write a function that returns both the minimum and maximum number of the given list/array.
 
-
-function minMax(arr){
-  let answer =[]
-  answer.push(Math.min(...arr), Math.max(...arr))
+function minMax(arr) {
+  let answer = [];
+  answer.push(Math.min(...arr), Math.max(...arr));
   // console.log(answer)
-  return answer
+  return answer;
 }
 
 // console.log(minMax([1, 2, 3, 4, 5]))
 // console.log(minMax([2334454, 5]))
 // console.log(minMax([5]))
 
-
-// Problem 3 
+// Problem 3
 
 // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
 function simpleMultiplication(number) {
-    if (number % 2 === 0) {
-      return number * 8
-    } else {
-      return number * 9
-    }
+  if (number % 2 === 0) {
+    return number * 8;
+  } else {
+    return number * 9;
+  }
 }
 
 // console.log(simpleMultiplication(2))
 // console.log(simpleMultiplication(5))
 // console.log(simpleMultiplication(10))
-// console.log(simpleMultiplication(13))  
+// console.log(simpleMultiplication(13))
 
 // problem 4
-
 
 // Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
 
 // It should remove all values from list a, which are present in list b keeping their order.
 
-
-// * I need to compare each array to the other and check if any numbers in the secound/b array are the same as any in the first/a array. 
-// If there is then I need to remove all of the repeating numbers from a 
-
+// * I need to compare each array to the other and check if any numbers in the secound/b array are the same as any in the first/a array.
+// If there is then I need to remove all of the repeating numbers from a
 
 function arrayDiff(a, b) {
-  let answer = []
+  let answer = [];
   for (let i = 0; i < a.length; i++) {
     if (b.length < 1) {
-      return a
+      return a;
     }
     for (let j = i + 1; j < b.length; j++) {
       if (a[i] === b[j]) {
-        a.splice(i, 1)
+        a.splice(i, 1);
       }
     }
   }
-  return a
+  return a;
 }
 
-
-// console.log(arrayDiff([1,2],[1])) // == [2]         
+// console.log(arrayDiff([1,2],[1])) // == [2]
 
 // ? Problem 5
 
@@ -118,24 +114,22 @@ function arrayDiff(a, b) {
 
 //? Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
 
-const binaryArrayToNumber = arr => {
-  let answer = 0
-  let powerOf = 0
+const binaryArrayToNumber = (arr) => {
+  let answer = 0;
+  let powerOf = 0;
   for (let i = arr.length - 1; i > -1; i--) {
     // console.log(i)
-   answer += arr[i] * Math.pow(2, powerOf)
-   ++powerOf
+    answer += arr[i] * Math.pow(2, powerOf);
+    ++powerOf;
   }
-    //  console.log('--------------')
-  return answer 
+  //  console.log('--------------')
+  return answer;
 };
 
 // console.log(binaryArrayToNumber([0, 0, 0, 1])) //* 1
 // console.log(binaryArrayToNumber([0, 0, 1, 0])) //* 2
 // console.log(binaryArrayToNumber([0, 1, 0, 1])) //* 5
 // console.log(binaryArrayToNumber([1, 0, 0, 1])) //* 9
-
-
 
 // ? Problem 6
 
@@ -145,55 +139,50 @@ const binaryArrayToNumber = arr => {
 // "double  spaces"      ==> "elbuod  secaps"
 
 function reverseWords(str) {
-  let reversed = ''
-  let newString = ''
-  let finalAns = ''
-  let joinedStr = ''
-newString = str.split(' ')
-// console.log(newString)
-let answer = newString.forEach(element => {
-  // console.log(element)
-  newString = element.split('')
+  let reversed = "";
+  let newString = "";
+  let finalAns = "";
+  let joinedStr = "";
+  newString = str.split(" ");
   // console.log(newString)
-   reversed = newString.reverse()
-  // console.log(reversed)
-  joinedStr = reversed.join('')
-  // console.log(joinedStr)
-   finalAns += joinedStr
-});
-  return finalAns
+  let answer = newString.forEach((element) => {
+    // console.log(element)
+    newString = element.split("");
+    // console.log(newString)
+    reversed = newString.reverse();
+    // console.log(reversed)
+    joinedStr = reversed.join("");
+    // console.log(joinedStr)
+    finalAns += joinedStr;
+  });
+  return finalAns;
 }
-
 
 // console.log(reverseWords("This is an example!"))
 // console.log(reverseWords("double  spaces"))
 
 // ! Come back
 
-
-// ? problem 7  // Finished! 
+// ? problem 7  // Finished!
 
 // ? An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
 
-
-
-function isIsogram(str){
-  let lowercaseStr = str.toLowerCase()
-  console.log(lowercaseStr)
+function isIsogram(str) {
+  let lowercaseStr = str.toLowerCase();
+  console.log(lowercaseStr);
   for (let i = 0; i < str.length; i++) {
     for (let k = i + 1; k < str.length; k++) {
       if (lowercaseStr[i] === lowercaseStr[k]) {
-          return false
+        return false;
       }
     }
   }
-  return true
+  return true;
 }
 
 // console.log(isIsogram("Dermatoglyphics"))       // * "Dermatoglyphics" = true
 // console.log(isIsogram("moose"))     // * "moose" = false
 // console.log(isIsogram("aba"))     // * "aba" = false
-
 
 //  ? Problem 8 Finished!
 
@@ -201,16 +190,15 @@ function isIsogram(str){
 
 // ? Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
 
-
-
-
-function lovefunc(flower1, flower2){
-  if (flower1 % 2 === 0 && flower2 % 2 !== 0 || flower2 % 2 === 0 && flower1 % 2 !== 0) {
-    return true
+function lovefunc(flower1, flower2) {
+  if (
+    (flower1 % 2 === 0 && flower2 % 2 !== 0) ||
+    (flower2 % 2 === 0 && flower1 % 2 !== 0)
+  ) {
+    return true;
+  } else {
+    return false;
   }
-   else { 
-    return false
-   }
 }
 
 // console.log(lovefunc(1,4))  // * true
@@ -218,21 +206,19 @@ function lovefunc(flower1, flower2){
 // console.log(lovefunc(0,1))  // * true
 // console.log(lovefunc(0,0))  // * false
 
-
-// ? Problem 9 
+// ? Problem 9
 
 // ? Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
 
-
-function duplicateCount(text){
-  let lowercase = text.toLowerCase()
-  let textArr = lowercase.split('')
+function duplicateCount(text) {
+  let lowercase = text.toLowerCase();
+  let textArr = lowercase.split("");
   let duplicates = textArr.filter((element, index) => {
-    return textArr.indexOf(element) !== index
-  })
+    return textArr.indexOf(element) !== index;
+  });
   // console.log(duplicates)
-  let answer = [...new Set(duplicates)]
-  return answer.length
+  let answer = [...new Set(duplicates)];
+  return answer.length;
 }
 
 // console.log(duplicateCount("")) // 0
@@ -242,15 +228,14 @@ function duplicateCount(text){
 // console.log(duplicateCount("Indivisibility")) // 1
 // console.log(duplicateCount("Indivisibilities")) // 2
 
-
-// ? Problem 10 
+// ? Problem 10
 // ? Very simple, given an integer or a floating-point number, find its opposite.
 
 function opposite(number) {
-  if(number === 0) {
-    return 0
+  if (number === 0) {
+    return 0;
   } else {
-    return number * -1
+    return number * -1;
   }
 }
 
@@ -260,30 +245,90 @@ function opposite(number) {
 // console.log(opposite(-5)) // * 5
 // console.log(opposite(4.24)) // * -4.25
 
-
-// ? Problem 11 
+// ? Problem 11
 
 // ? In this kata you are required to, given a string, replace every letter with its position in the alphabet.
 
 // ? If anything in the text isn't a letter, ignore it and don't return it.
 
-let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+let alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
 function alphabetPosition(text) {
-  const lowerCase = text.toLowerCase().split(' ').join('')
-  let answer = []
-  let char = ""
+  const lowerCase = text.toLowerCase().split(" ").join("");
+  let answer = [];
+  let char = "";
   // console.log(lowerCase)
-    for (let i = 0; i < lowerCase.length; i++) {
-      char = lowerCase.charAt(i)
-      if (alphabet.indexOf(char) > - 1) {
-        answer.push(alphabet.indexOf(char) + 1)
-      }
+  for (let i = 0; i < lowerCase.length; i++) {
+    char = lowerCase.charAt(i);
+    if (alphabet.indexOf(char) > -1) {
+      answer.push(alphabet.indexOf(char) + 1);
     }
-    let finalAnswer = answer.join(' ')
-  return finalAnswer
+  }
+  let finalAnswer = answer.join(" ");
+  return finalAnswer;
 }
 
-
 // console.log(alphabetPosition("The sunset sets at twelve o' clock.") // * "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+
+// ? Problem 12
+
+// ? Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+// ? When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+// ? Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+// ? S is misinterpreted as 5
+//  ?O is misinterpreted as 0
+// ? I is misinterpreted as 1
+// ? The test cases contain numbers only by mistake.
+
+function correct(string) {
+  let arr = string.split("");
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '5') {
+      arr.splice(i, 1, "S");
+    } else if (arr[i] === '0') {
+      arr.splice(i, 1, "O");
+    } else if (arr[i] === '1') {
+      arr.splice(i, 1, "I");
+    }
+  }
+  let answer = arr.join("");
+  return answer;
+}
+
+// console.log(correct("L0ND0N")); // *"LONDON"
+// console.log(correct("DUBL1N")); // *"DUBLIN"
+// console.log(correct("51NGAP0RE")); //*"SINGAPORE"
+// console.log(correct("BUDAPE5T")) // *"BUDAPEST"
+// console.log(correct("PAR15")) //* "PARIS"
+
+
