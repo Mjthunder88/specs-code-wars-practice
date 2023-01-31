e7aa831e2adf19db74185e4ae0396f2d64f4fc64
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-01-31 11:11:48 -0700

    finished problem 15

--- index.js
+++ index.js
@@ -419,3 +419,20 @@ function getDivisorsCnt(n){
 // console.log(getDivisorsCnt(5))  // * 2 (1, 5)
 // console.log(getDivisorsCnt(12)) // * 6 (1, 2, 3, 4, 6, 12)
 // console.log(getDivisorsCnt(30)) // * 8 (1, 2, 3, 5, 6, 10, 15, 30)
+
+
+// ? problem 15
+
+
+// ? You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+// ? Write a program that returns the girl's age (0-9) as an integer.
+
+// ? Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+
+function getAge(inputString){
+  let answer = parseInt(inputString)
+  return answer
+  }
+
+  console.log(getAge("4 years old")) //, 4
