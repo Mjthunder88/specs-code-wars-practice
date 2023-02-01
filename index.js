d8df4a6b05fa4f2c1eaf2667166f77c579d5b231
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-02-01 11:17:28 -0700

    finished problem 16

--- index.js
+++ index.js
@@ -435,4 +435,29 @@ function getAge(inputString){
   return answer
   }
 
-  console.log(getAge("4 years old")) //, 4
+  // console.log(getAge("4 years old")) //, 4
+
+
+  // ? problem 16
+
+// ?   There is a bus moving in the city which takes and drops some people at each bus stop.
+// ? You are provided with a list (or array) of integer pairs. Elements of each pair represent the number of people that get on the bus (the first item) and the number of people that get off the bus (the second item) at a bus stop.
+// ? Your task is to return the number of people who are still on the bus after the last bus stop (after the last array). Even though it is the last bus stop, the bus might not be empty and some people might still be inside the bus, they are probably sleeping there :D
+
+// ? The second value in the first pair in the array is 0, since the bus is empty in the first bus stop.
+
+
+  var number = function(busStops){
+    // console.log(busStops[0][0])
+    let totalPeople = 0
+    for (let i = 0; i < busStops.length; i++) {
+      totalPeople += busStops[i][0]
+      totalPeople -= busStops[i][1]
+    }
+    return totalPeople 
+  }
+
+  console.log(number([[10,0],[3,5],[5,8]])) // *,5;
+  console.log(number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]])) // *,17;
+  console.log(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]])) // *,21;
+  console.log(number([[0,0]])) // *,0
