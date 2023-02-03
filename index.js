17ee64ad660b2395a55c34c782c8eb7f906cbc55
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-02-03 11:44:16 -0700

    finisehd problem 20

--- index.js
+++ index.js
@@ -527,3 +527,21 @@ function basicOp(operation, value1, value2) {
   }
 }
 
+
+// ? problem 20
+
+function points(games) {
+  let points = 0
+  games.forEach((element, index) => {
+    if (+element[0] > +element[2]) {
+      points += 3
+    } else if (+element[0] < +element[2]) {
+      points +=  0
+    } else if (+element[0] === +element[2]) {
+      points += 1
+    }
+  })
+  return points
+}
+
+console.log(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"])) // * 30
