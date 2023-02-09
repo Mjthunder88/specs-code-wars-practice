151e8a1c21238c8c13f1486609044c11e9194efd
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-02-09 10:57:03 -0700

    finished a codeWars

--- index.js
+++ index.js
@@ -544,4 +544,24 @@ function points(games) {
   return points
 }
 
-console.log(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"])) // * 30
+// console.log(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"])) // * 30
+
+// ? Problem 21
+
+// ? You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+// ? Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+// ? Function should return true if it is possible and false if not.
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  let milesLeft = mpg * fuelLeft
+  if (milesLeft < distanceToPump) {
+    return false
+  } else {
+    return true
+  }
+};
+
+console.log(zeroFuel(50, 25, 2)) // *true
+console.log(zeroFuel(100, 50, 1)) // * false
