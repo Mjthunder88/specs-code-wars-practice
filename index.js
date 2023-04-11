8c5e6880a635f9bf0560bcaf4a511c655c04e7ef
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-04-10 21:47:44 -0600

    finished problem 59 about charactercodes

--- index.js
+++ index.js
@@ -1252,3 +1252,21 @@ function reverseWords(str) {
 // console.log(reverseWords('apple')) // ?, 'elppa');
 // console.log(reverseWords('a b c d')) // ?, 'a b c d');
 // console.log(reverseWords('double  spaced  words')) // ?, 'elbuod  decaps  sdrow');
+
+// ? Probelm 59
+
+function getChar(c) {
+  return String.fromCharCode(c);
+}
+
+// console.log(getChar(55)) // *,'7')
+// console.log(getChar(56)) // *,'8')
+// console.log(getChar(57)) // *,'9')
+// console.log(getChar(58)) // *,':')
+// console.log(getChar(59)) // *,';')
+// console.log(getChar(60)) // *,'<')
+// console.log(getChar(61)) // *,'=')
+// console.log(getChar(62)) // *,'>')
+// console.log(getChar(63)) // *,'?')
+// console.log(getChar(64)) // *,'@')
+// console.log(getChar(65)) // *,'A')
