d76e4861284b408511101e807e6ba71350cfddce
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-07-06 15:38:49 -0600

    finished problem 68

--- index.js
+++ index.js
@@ -1456,3 +1456,17 @@ function chromosomeCheck(sperm) {
     return "Congratulations! You're going to have a son."
   }
 }
+
+
+// ? Problem 68
+
+function hexToDec(hexString){
+  return parseInt(hexString, 16)
+}
+
+
+    console.log(hexToDec("1")) // *, 1);
+    console.log(hexToDec("a")) // *, 10);
+    console.log(hexToDec("10")) // *, 16);
+    console.log(hexToDec("FF")) // *, 255);
+    console.log(hexToDec("-C")) // *, -12);
