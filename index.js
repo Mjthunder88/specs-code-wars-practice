dc9e5d726564e4910a0eb6c21173984d23bd859d
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-07-06 15:25:27 -0600

    finished problem 67

--- index.js
+++ index.js
@@ -1441,7 +1441,18 @@ function removeSmallest(numbers) {
 }
 
 
-console.log(removeSmallest([1, 2, 3, 4, 5])) // *, [2, 3, 4, 5], "Wrong result for [1, 2, 3, 4, 5]");
-console.log(removeSmallest([5, 3, 2, 1, 4])) // *, [5, 3, 2, 4], "Wrong result for [5, 3, 2, 1, 4]");
-console.log(removeSmallest([2, 2, 1, 2, 1])) // *, [2, 2, 2, 1], "Wrong result for [2, 2, 1, 2, 1]");
-console.log(removeSmallest([])) // *, [], "Wrong result for []")
+// console.log(removeSmallest([1, 2, 3, 4, 5])) // *, [2, 3, 4, 5], "Wrong result for [1, 2, 3, 4, 5]");
+// console.log(removeSmallest([5, 3, 2, 1, 4])) // *, [5, 3, 2, 4], "Wrong result for [5, 3, 2, 1, 4]");
+// console.log(removeSmallest([2, 2, 1, 2, 1])) // *, [2, 2, 2, 1], "Wrong result for [2, 2, 1, 2, 1]");
+// console.log(removeSmallest([])) // *, [], "Wrong result for []")
+
+
+// ? Problem 67
+
+function chromosomeCheck(sperm) {
+  if (sperm === 'XX') {
+    return "Congratulations! You're going to have a daughter."
+  } else {
+    return "Congratulations! You're going to have a son."
+  }
+}
